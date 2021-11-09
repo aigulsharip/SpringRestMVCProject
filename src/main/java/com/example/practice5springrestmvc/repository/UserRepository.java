301a1e1d9611b212614bc328b2dcf3229b0e97a7
aigulsharip <aigul.sharip@nu.edu.kz>
2021-11-09 11:36:47 +0600

    practice5 is completed

--- src/main/java/com/example/practice5springrestmvc/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.practice5springrestmvc.repository;
-
-import com.example.practice5springrestmvc.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
